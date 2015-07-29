d56912f696f2032af5beebaa1bbd60b00b572df3
植木善弘 <pirori67@gmail.com>
2015-07-30 08:44:13 +0900

    Home Pageで入力したtext表示に成功した。

--- src/main/java/com/mycompany/myproject/HomePage.java
+++ src/main/java/com/mycompany/myproject/HomePage.java
@@ -2,28 +2,144 @@ package com.mycompany.myproject;
 
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
+	private Model mOrderingCompany = new Model();
+	private Form form = new Form("form") {
+		private static final long serialVersionUID = -1700095884500348972L;
+
+		@Override
+		protected void onSubmit() {
+			PrintPage result = new PrintPage((HomePage) this.getParent());
+			setResponsePage(result);
+		}
+	};
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
+		add(form);
 
-		//add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-		Form form = new Form("form");
 		Button nextButton = new Button("nextButton") {
 			private static final long serialVersionUID = 1L;
 			public void onSubmit() {
-				setResponsePage(PrintPage.class);
+//				PrintPage resultPrintPage = new PrintPage((HomePage) this.getParent());
+//				setResponsePage(resultPrintPage);
+//				setResponsePage((HomePage) this.getParent());
 			}
 		};
 		form.add(nextButton);
-		super.add(form);
+//		super.add(form);
+
+		// ここから追加分
+
+		// フォームにテキストフィールドを追加
+		final TextField field = new TextField("orderingCompany", mOrderingCompany);
+		field.setOutputMarkupId(true);
+		form.add(field);
+
+//		super.add(form);
 
 		// TODO Add your page's components here
 
     }
+
+	public Model getMOrderingCompany() {
+		return mOrderingCompany;
+	}
 }
+
+/*
+package com.mycompany.myproject;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+//import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+public class HomePage extends WebPage {
+
+	private static final long serialVersionUID = 3598788934381254916L;
+
+	private Form form = new Form("f") {
+		private static final long serialVersionUID = -1700095884500348972L;
+
+		@Override
+		protected void onSubmit() {
+			PrintPage result = new PrintPage((HomePage) this.getParent());
+			setResponsePage(result);
+		}
+	};
+
+	private Model mSubject = new Model();
+
+	private Model mType = new Model();
+
+	private Model mDate = new Model(new Date());
+
+	private Model mArticle = new Model();
+
+	private FeedbackPanel feedback = new FeedbackPanel("msg");
+
+	public HomePage(final PageParameters parameters) {
+
+		add(feedback);
+
+		add(form);
+
+		TextField txtSubject = new TextField("subject", mSubject);
+		txtSubject.setRequired(true);
+		form.add(txtSubject);
+
+		List<String> typeList = new ArrayList<String>();
+		typeList.add("Diary");
+		typeList.add("Book Review");
+		typeList.add("Tech Memo");
+		typeList.add("Diet");
+		// If you want to set the Default Value of combo box,
+		// write like following.
+		//mType.setObject("Diary");
+		DropDownChoice cmbType =new DropDownChoice("type", mType, typeList);
+		cmbType.setRequired(true);
+		form.add(cmbType);
+
+		TextField txtDate = new TextField("date", mDate,Date.class);
+		txtDate.setRequired(true);
+		form.add(txtDate);
+
+		TextArea txtArticle = new TextArea("article", mArticle);
+		txtArticle.setRequired(true);
+		form.add(txtArticle);
+	}
+
+	public Model getMSubject() {
+		return mSubject;
+	}
+
+	public Model getMType() {
+		return mType;
+	}
+
+	public Model getMDate() {
+		return mDate;
+	}
+
+	public Model getMArticle() {
+		return mArticle;
+	}
+}
+
+*/
--- src/main/java/com/mycompany/myproject/PrintPage.java
+++ src/main/java/com/mycompany/myproject/PrintPage.java
@@ -8,8 +8,48 @@ import org.apache.wicket.markup.html.basic.Label;
  */
 public class PrintPage extends WebPage{
     private static final long serialVersionUID = 1L;
+    private WebPage formerPage;
 
-    public PrintPage() {
-        super.add(new Label("nextMessage", "これは2ページ目です。"));
+    public PrintPage(HomePage page) {
+        this.formerPage=page;
+        add(new Label("nextMessage", "これは2ページ目です。"));
+        add(new Label("orderingCompanyValue", page.getMOrderingCompany()));
+    }
+
+    public void formerPage(HomePage page){
+        formerPage = page;
     }
 }
+
+
+/*
+package com.mycompany.myproject;
+
+        import org.apache.wicket.markup.html.WebPage;
+        import org.apache.wicket.markup.html.basic.Label;
+        import org.apache.wicket.markup.html.form.Form;
+        import org.apache.wicket.markup.html.form.TextArea;
+
+public class PrintPage extends WebPage {
+    private static final long serialVersionUID = -8680953158566804713L;
+    private WebPage formerPage;
+    private Form form = new Form("f") {
+
+        private static final long serialVersionUID = 5522440160951241211L;
+
+        @Override
+        protected void onSubmit() {
+            setResponsePage(formerPage);
+        }
+    };
+
+    public PrintPage(HomePage page) {
+        this.formerPage=page;
+        add(new Label("subject",page.getMSubject()));
+        add(new Label("type",page.getMType()));
+        add(new Label("date",page.getMDate()));
+        add(new TextArea("article",page.getMArticle()));
+        add(form);
+    }
+}
+ */
