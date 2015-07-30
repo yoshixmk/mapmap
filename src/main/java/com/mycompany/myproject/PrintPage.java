970ea0507f3806c612ecdf75c0f314541f9f58ba
植木善弘 <pirori67@gmail.com>
2015-07-30 19:17:27 +0900

    modified:HomePage.html,HomePage.java,PrintPage.html,PrintPage.java

--- src/main/java/com/mycompany/myproject/PrintPage.java
+++ src/main/java/com/mycompany/myproject/PrintPage.java
@@ -42,36 +42,3 @@ public class PrintPage extends WebPage{
         formerPage = page;
     }
 }
-
-
-/*
-package com.mycompany.myproject;
-
-        import org.apache.wicket.markup.html.WebPage;
-        import org.apache.wicket.markup.html.basic.Label;
-        import org.apache.wicket.markup.html.form.Form;
-        import org.apache.wicket.markup.html.form.TextArea;
-
-public class PrintPage extends WebPage {
-    private static final long serialVersionUID = -8680953158566804713L;
-    private WebPage formerPage;
-    private Form form = new Form("f") {
-
-        private static final long serialVersionUID = 5522440160951241211L;
-
-        @Override
-        protected void onSubmit() {
-            setResponsePage(formerPage);
-        }
-    };
-
-    public PrintPage(HomePage page) {
-        this.formerPage=page;
-        add(new Label("subject",page.getMSubject()));
-        add(new Label("type",page.getMType()));
-        add(new Label("date",page.getMDate()));
-        add(new TextArea("article",page.getMArticle()));
-        add(form);
-    }
-}
- */
