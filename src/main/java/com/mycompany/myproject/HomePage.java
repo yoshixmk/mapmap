5f8edbb8548dbd435d6206c2ce16163cf6b8dcab
植木善弘 <pirori67@gmail.com>
2015-07-30 15:53:35 +0900

    プリントページに全ての項目を表示できるようにした

--- src/main/java/com/mycompany/myproject/HomePage.java
+++ src/main/java/com/mycompany/myproject/HomePage.java
@@ -11,6 +11,28 @@ import org.apache.wicket.markup.html.WebPage;
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 	private Model mOrderingCompany = new Model();
+	private Model mOrderingCompanyAddress = new Model();
+	private Model mOrderingContactPersonName = new Model();
+	private Model mProductItem1 = new Model();
+	private Model mProductItem2 = new Model();
+	private Model mProductItem3 = new Model();
+	private Model mProductItem4 = new Model();
+	private Model mProductItem5 = new Model();
+	private Model mQuantity1 = new Model();
+	private Model mQuantity2 = new Model();
+	private Model mQuantity3 = new Model();
+	private Model mQuantity4 = new Model();
+	private Model mQuantity5 = new Model();
+	private Model mUnitPrice1 = new Model();
+	private Model mUnitPrice2 = new Model();
+	private Model mUnitPrice3 = new Model();
+	private Model mUnitPrice4 = new Model();
+	private Model mUnitPrice5 = new Model();
+	private Model mAmountOfMoney1 = new Model();
+	private Model mAmountOfMoney2 = new Model();
+	private Model mAmountOfMoney3 = new Model();
+	private Model mAmountOfMoney4 = new Model();
+	private Model mAmountOfMoney5 = new Model();
 	private Form form = new Form("form") {
 		private static final long serialVersionUID = -1700095884500348972L;
 
@@ -39,11 +61,76 @@ public class HomePage extends WebPage {
 		// ここから追加分
 
 		// フォームにテキストフィールドを追加
-		final TextField field = new TextField("orderingCompany", mOrderingCompany);
+		TextField field = new TextField("orderingCompany", mOrderingCompany);
 		field.setOutputMarkupId(true);
-		form.add(field);
+		form.add(field);//受注先会社名
+		field = new TextField("orderingCompanyAddress", mOrderingCompanyAddress);
+		field.setOutputMarkupId(true);
+		form.add(field);//住所
+		field = new TextField("orderingContactPersonName", mOrderingContactPersonName);
+		field.setOutputMarkupId(true);
+		form.add(field);//受注先担当者名
+		field = new TextField("productItem1", mProductItem1);
+		field.setOutputMarkupId(true);
+		form.add(field);//項目
+		field = new TextField("productItem2", mProductItem2);
+		field.setOutputMarkupId(true);
+		form.add(field);//項目
+		field = new TextField("productItem3", mProductItem3);
+		field.setOutputMarkupId(true);
+		form.add(field);//項目
+		field = new TextField("productItem4", mProductItem4);
+		field.setOutputMarkupId(true);
+		form.add(field);//項目
+		field = new TextField("productItem5", mProductItem5);
+		field.setOutputMarkupId(true);
+		form.add(field);//項目
+		field = new TextField("quantity1", mQuantity1);
+		field.setOutputMarkupId(true);
+		form.add(field);//数量
+		field = new TextField("quantity2", mQuantity2);
+		field.setOutputMarkupId(true);
+		form.add(field);//数量
+		field = new TextField("quantity3", mQuantity3);
+		field.setOutputMarkupId(true);
+		form.add(field);//数量
+		field = new TextField("quantity4", mQuantity4);
+		field.setOutputMarkupId(true);
+		form.add(field);//数量
+		field = new TextField("quantity5", mQuantity5);
+		field.setOutputMarkupId(true);
+		form.add(field);//数量
+		field = new TextField("unitPrice1", mUnitPrice1);
+		field.setOutputMarkupId(true);
+		form.add(field);//単価
+		field = new TextField("unitPrice2", mUnitPrice2);
+		field.setOutputMarkupId(true);
+		form.add(field);//単価
+		field = new TextField("unitPrice3", mUnitPrice3);
+		field.setOutputMarkupId(true);
+		form.add(field);//単価
+		field = new TextField("unitPrice4", mUnitPrice4);
+		field.setOutputMarkupId(true);
+		form.add(field);//単価
+		field = new TextField("unitPrice5", mUnitPrice5);
+		field.setOutputMarkupId(true);
+		form.add(field);//単価
+		field = new TextField("amountOfMoney1", mAmountOfMoney1);
+		field.setOutputMarkupId(true);
+		form.add(field);//金額
+		field = new TextField("amountOfMoney2", mAmountOfMoney2);
+		field.setOutputMarkupId(true);
+		form.add(field);//金額
+		field = new TextField("amountOfMoney3", mAmountOfMoney3);
+		field.setOutputMarkupId(true);
+		form.add(field);//金額
+		field = new TextField("amountOfMoney4", mAmountOfMoney4);
+		field.setOutputMarkupId(true);
+		form.add(field);//金額
+		field = new TextField("amountOfMoney5", mAmountOfMoney5);
+		field.setOutputMarkupId(true);
+		form.add(field);//金額
 
-//		super.add(form);
 
 		// TODO Add your page's components here
 
@@ -51,6 +138,72 @@ public class HomePage extends WebPage {
 
 	public Model getMOrderingCompany() {
 		return mOrderingCompany;
+	}
+	public Model getMOrderingCompanyAddress() {
+		return mOrderingCompanyAddress;
+	}
+	public Model getMOrderingContactPersonName() {
+		return mOrderingContactPersonName;
+	}
+	public Model getMProductItem1() {
+		return mProductItem1;
+	}
+	public Model getMProductItem2() {
+		return mProductItem2;
+	}
+	public Model getMProductItem3() {
+		return mProductItem3;
+	}
+	public Model getMProductItem4() {
+		return mProductItem4;
+	}
+	public Model getMProductItem5() {
+		return mProductItem5;
+	}
+	public Model getMQuantity1() {
+		return mQuantity1;
+	}
+	public Model getMQuantity2() {
+		return mQuantity2;
+	}
+	public Model getMQuantity3() {
+		return mQuantity3;
+	}
+	public Model getMQuantity4() {
+		return mQuantity4;
+	}
+	public Model getMQuantity5() {
+		return mQuantity5;
+	}
+	public Model getMUnitPrice1() {
+		return mUnitPrice1;
+	}
+	public Model getMUnitPrice2() {
+		return mUnitPrice2;
+	}
+	public Model getMUnitPrice3() {
+		return mUnitPrice3;
+	}
+	public Model getMUnitPrice4() {
+		return mUnitPrice4;
+	}
+	public Model getMUnitPrice5() {
+		return mUnitPrice5;
+	}
+	public Model getMAmountOfMoney1() {
+		return mAmountOfMoney1;
+	}
+	public Model getMAmountOfMoney2() {
+		return mAmountOfMoney2;
+	}
+	public Model getMAmountOfMoney3() {
+		return mAmountOfMoney3;
+	}
+	public Model getMAmountOfMoney4() {
+		return mAmountOfMoney4;
+	}
+	public Model getMAmountOfMoney5() {
+		return mAmountOfMoney5;
 	}
 }
 
--- src/main/java/com/mycompany/myproject/PrintPage.java
+++ src/main/java/com/mycompany/myproject/PrintPage.java
@@ -14,6 +14,28 @@ public class PrintPage extends WebPage{
         this.formerPage=page;
         add(new Label("nextMessage", "これは2ページ目です。"));
         add(new Label("orderingCompanyValue", page.getMOrderingCompany()));
+        add(new Label("orderingCompanyAddressValue", page.getMOrderingCompanyAddress()));
+        add(new Label("orderingContactPersonNameValue", page.getMOrderingContactPersonName()));
+        add(new Label("productItemValue1", page.getMProductItem1()));
+        add(new Label("productItemValue2", page.getMProductItem2()));
+        add(new Label("productItemValue3", page.getMProductItem3()));
+        add(new Label("productItemValue4", page.getMProductItem4()));
+        add(new Label("productItemValue5", page.getMProductItem5()));
+        add(new Label("quantityValue1", page.getMQuantity1()));
+        add(new Label("quantityValue2", page.getMQuantity2()));
+        add(new Label("quantityValue3", page.getMQuantity3()));
+        add(new Label("quantityValue4", page.getMQuantity4()));
+        add(new Label("quantityValue5", page.getMQuantity5()));
+        add(new Label("unitPriceValue1", page.getMUnitPrice1()));
+        add(new Label("unitPriceValue2", page.getMUnitPrice2()));
+        add(new Label("unitPriceValue3", page.getMUnitPrice3()));
+        add(new Label("unitPriceValue4", page.getMUnitPrice4()));
+        add(new Label("unitPriceValue5", page.getMUnitPrice5()));
+        add(new Label("amountOfMoneyValue1", page.getMAmountOfMoney1()));
+        add(new Label("amountOfMoneyValue2", page.getMAmountOfMoney2()));
+        add(new Label("amountOfMoneyValue3", page.getMAmountOfMoney3()));
+        add(new Label("amountOfMoneyValue4", page.getMAmountOfMoney4()));
+        add(new Label("amountOfMoneyValue5", page.getMAmountOfMoney5()));
     }
 
     public void formerPage(HomePage page){
