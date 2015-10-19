326907aa58abba3e4c2dd135230a8a5f3d3b27f1
植木善弘 <pirori67@gmail.com>
2015-10-19 23:12:33 +0900

    ProductItem class change Serializable.

--- src/main/java/com/mycompany/myproject/ProductItem.java
+++ src/main/java/com/mycompany/myproject/ProductItem.java
@@ -2,14 +2,15 @@ package com.mycompany.myproject;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by yoshihiro on 2015/10/19.
  */
 @Data
-public class ProductItem {
+public class ProductItem implements Serializable{
     private String productItem;
     private String quantity;
     private String unitPrice;
     private String subtotalPrice;
 }
-
