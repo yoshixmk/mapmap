fd47cc4e478c1a38b7d08dd5035c08d784a42a96
植木善弘 <pirori67@gmail.com>
2015-10-19 11:04:50 +0900

    product item object 1~5 added. create <table>. comp

--- src/main/java/com/mycompany/myproject/PrintPage.java
+++ src/main/java/com/mycompany/myproject/PrintPage.java
@@ -69,18 +69,51 @@ public class PrintPage extends WebPage{
             add(new Label("calcAmountOfMoney5", "５の単価または数量に空欄があります"));
         }
 
-        ArrayList<String> list =
-                new ArrayList<String>();
-        list.add("ユーザー1");
-        list.add("ユーザー2");
-        list.add("ユーザー3");
-        add(new ListView<String>("list", list) {
-                    @Override
-                    protected void populateItem(ListItem<String> item) {
-                        String name = item.getModelObject();
-                        item.add(new Label("name", name));
-                    }
-                });
+        ArrayList<ProductItem> productItemList = new ArrayList<ProductItem>();
+        if(page.getMProductItem1().getObject()!=null && page.getMQuantity1().getObject()!=null && page.getMUnitPrice1().getObject()!=null) {
+            ProductItem item1 = new ProductItem();
+            item1.setProductItem(page.getMProductItem1().getObject().toString());
+            item1.setQuantity(page.getMQuantity1().getObject().toString());
+            item1.setUnitPrice(page.getMUnitPrice1().getObject().toString());
+            productItemList.add(item1);
+        }
+        if(page.getMProductItem2().getObject()!=null && page.getMQuantity2().getObject()!=null && page.getMUnitPrice2().getObject()!=null) {
+            ProductItem item2 = new ProductItem();
+            item2.setProductItem(page.getMProductItem2().getObject().toString());
+            item2.setQuantity(page.getMQuantity2().getObject().toString());
+            item2.setUnitPrice(page.getMUnitPrice2().getObject().toString());
+            productItemList.add(item2);
+        }
+        if(page.getMProductItem3().getObject()!=null && page.getMQuantity3().getObject()!=null && page.getMUnitPrice3().getObject()!=null) {
+            ProductItem item3 = new ProductItem();
+            item3.setProductItem(page.getMProductItem3().getObject().toString());
+            item3.setQuantity(page.getMQuantity3().getObject().toString());
+            item3.setUnitPrice(page.getMUnitPrice3().getObject().toString());
+            productItemList.add(item3);
+        }
+        if(page.getMProductItem4().getObject()!=null && page.getMQuantity4().getObject()!=null && page.getMUnitPrice4().getObject()!=null) {
+            ProductItem item4 = new ProductItem();
+            item4.setProductItem(page.getMProductItem4().getObject().toString());
+            item4.setQuantity(page.getMQuantity4().getObject().toString());
+            item4.setUnitPrice(page.getMUnitPrice4().getObject().toString());
+            productItemList.add(item4);
+        }
+        if(page.getMProductItem5().getObject()!=null && page.getMQuantity5().getObject()!=null && page.getMUnitPrice5().getObject()!=null) {
+            ProductItem item5 = new ProductItem();
+            item5.setProductItem(page.getMProductItem5().getObject().toString());
+            item5.setQuantity(page.getMQuantity5().getObject().toString());
+            item5.setUnitPrice(page.getMUnitPrice5().getObject().toString());
+            productItemList.add(item5);
+        }
+        add(new ListView<ProductItem>("productItemList", productItemList) {
+            @Override
+            protected void populateItem(ListItem<ProductItem> item) {
+                ProductItem productItem = item.getModelObject();
+                item.add(new Label("productItem", productItem.getProductItem()));
+                item.add(new Label("quantity", productItem.getQuantity()));
+                item.add(new Label("unitPrice", productItem.getUnitPrice()));
+            }
+        });
     }
 
     public Integer calcAmountOfMoney(Model quantityValue, Model unitPriceValue){
---
+++ src/main/java/com/mycompany/myproject/ProductItem.java
@@ -0,0 +1,14 @@
+package com.mycompany.myproject;
+
+import lombok.Data;
+
+/**
+ * Created by yoshihiro on 2015/10/19.
+ */
+@Data
+public class ProductItem {
+    private String productItem;
+    private String quantity;
+    private String unitPrice;
+}
+
