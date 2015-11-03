4c20f05f27bf4e371533dd0df153e7b609a9b546
植木善弘 <pirori67@gmail.com>
2015-11-03 22:27:25 +0900

    validation message added.

--- src/main/java/com/mycompany/myproject/HomePage.java
+++ src/main/java/com/mycompany/myproject/HomePage.java
@@ -1,24 +1,32 @@
 package com.mycompany.myproject;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.feedback.ExactLevelFeedbackMessageFilter;
+import org.apache.wicket.feedback.FeedbackMessage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.form.validation.FormComponentFeedbackBorder;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.validation.ValidationError;
+import org.apache.wicket.validation.validator.RangeValidator;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
-	private Model mOrderingCompany = new Model();
-	private Model mOrderingCompanyAddress = new Model();
-	private Model mOrderingContactPersonName = new Model();
+	private Model<String> mOrderingCompany = new Model<String>();
+	private Model<String> mOrderingCompanyAddress = new Model<String>();
+	private Model<String> mOrderingContactPersonName = new Model<String>();
 	private ArrayList<ProductItem> productItemList = new ArrayList<ProductItem>();
 
 	private Form form = new Form("form") {
@@ -43,30 +51,35 @@ public class HomePage extends WebPage {
 		form.add(nextButton);
 
 		// フォームにテキストフィールドを追加
-		TextField field = new TextField("orderingCompany", mOrderingCompany);
+		TextField field = new TextField<String>("orderingCompany", mOrderingCompany);
 		field.setOutputMarkupId(true);
 		form.add(field);//受注先会社名
-		field = new TextField("orderingCompanyAddress", mOrderingCompanyAddress);
+		field = new TextField<String>("orderingCompanyAddress", mOrderingCompanyAddress);
 		field.setOutputMarkupId(true);
 		form.add(field);//住所
-		field = new TextField("orderingContactPersonName", mOrderingContactPersonName);
+		field = new TextField<String>("orderingContactPersonName", mOrderingContactPersonName);
 		field.setOutputMarkupId(true);
 		form.add(field);//受注先担当者名
 		for(int i=0; i<5; i++) {
 			productItemList.add(new ProductItem());
 		}
-		form.add(new ListView<ProductItem>("productItemList", productItemList) {
+		form.add(new FeedbackPanel("feedbackMessage",
+				new ExactLevelFeedbackMessageFilter(FeedbackMessage.ERROR)));
+//		form.add(new FeedbackPanel("succesMessage",
+//				new ExactLevelFeedbackMessageFilter(FeedbackMessage.SUCCESS)));
+		ListView<ProductItem> listView = new ListView<ProductItem>("productItemList", productItemList) {
 			@Override
 			protected void populateItem(ListItem<ProductItem> listItem) {
 				ProductItem productItem = listItem.getModelObject();
 				listItem.add(new TextField<String>("productItemName", new PropertyModel<String>(productItem, "productItemName")));
-				listItem.add(new TextField<Integer>("quantity", new PropertyModel<Integer>(productItem, "quantity")));
-				listItem.add(new TextField<Integer>("unitPrice", new PropertyModel<Integer>(productItem, "unitPrice")));
+				listItem.add(new TextField<Integer>("quantity", new PropertyModel<Integer>(productItem, "quantity"))
+						.add(RangeValidator.minimum(0)));
+				listItem.add(new TextField<Integer>("unitPrice", new PropertyModel<Integer>(productItem, "unitPrice"))
+						.add(RangeValidator.minimum(0)));
 			}
-		});
-
-		// TODO Add your page's components here
-
+		};
+		listView.setReuseItems(true);
+		form.add(listView);
     }
 
 	public Model getMOrderingCompany() {
